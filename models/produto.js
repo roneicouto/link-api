a9685675b1999308be004dd20cc1e5c5c43b0142
josenilson <josenilson.neiva@gmail.com>
2019-02-11 13:01:37 -0300

    Correção dos métodos para busca de Estoques e Preços

--- models/produto.js
+++ models/produto.js
@@ -59,103 +59,122 @@ module.exports = class Produto extends Cadastro {
   }
 
 
-  static async precoVenda(idProduto, idLoja, idTabela) {
-    let params, sql, result
-    params = [idProduto, idLoja]
-    if (idTabela) params.push(idTabela)
-    sql = `SELECT 
-            id_tab_preco, 
-            nome_tab_preco,
-            preco_venda,
-            preco_promocao
-          FROM 
-            vs_api_produtos_precos 
-          WHERE 
-            id_produto = $1 and id_loja = $2
-            ${ idTabela ? 'and id_tab_preco = $3' : '' }
-          ORDER BY
-            id_tab_preco`
-    result = await db.query(sql, params)
-    return ! idTabela ? result.rows : result.rows.length > 0 ? result.rows[0] : {}
+  static precoVenda(idProduto, idLoja, idTabela) {
+    return new Promise((resolve, reject) =>{
+      let params, sql
+      params = [idProduto, idLoja]
+      if (idTabela) params.push(idTabela)
+      sql = `SELECT 
+              id_tab_preco, 
+              nome_tab_preco,
+              preco_venda,
+              preco_promocao
+            FROM 
+              vs_api_produtos_precos 
+            WHERE 
+              id_produto = $1 and id_loja = $2
+              ${ idTabela ? 'and id_tab_preco = $3' : '' }
+            ORDER BY
+              id_tab_preco`
+      db.query(sql, params)
+        .then(({rows}) => resolve(! idTabela ? rows : rows.length > 0 ? rows[0] : {}))
+        .catch(error => reject(error))
+    })
   }
 
-  static async precoTabela(idProduto, idLoja, idTabela) {
-    let resp = await Produto.precoVenda(idProduto, idLoja, idTabela)
-    return resp.preco_venda
-  }
-
-
-  static async precoVendaPlano(idProduto, idLoja, idTabela, idPlano) {
-    let params, sql, result
-    params = [idProduto, idLoja, idTabela]
-    if (idPlano) params.push(idPlano)
-    sql = `SELECT 
-            id_plano_pag, 
-            nome_plano_pag,
-            forma_pag,
-            preco_venda
-          FROM 
-            vs_api_produtos_precos_planos 
-          WHERE 
-            id_produto = $1 and id_loja = $2 and id_tab_preco = $3
-            ${ idPlano ? 'and id_plano_pag = $4' : '' }
-          ORDER BY
-            id_plano_pag`
-    result = await db.query(sql, params)
-    return ! idPlano ? result.rows : result.rows.length > 0 ? result.rows[0] : {} 
+  static precoTabela(idProduto, idLoja, idTabela) {
+    return new Promise((resolve, reject) => {
+      Produto.precoVenda(idProduto, idLoja, idTabela)
+        .then(preco => resolve( preco.preco_venda || 0))
+        .catch(error => reject(error))
+    })
   }
 
 
-  static async estoqueLoja(idProduto, idLoja) {
-    let params, sql, result
-    params = [idProduto]
-    if (idLoja) params.push(idLoja)
-    sql = `SELECT 
-            id_loja, 
-            nome_loja,
-            saldo_loja,
-            saldo_dep,
-            saldo_loja_frc,
-            saldo_dep_frc
-          FROM 
-            vs_api_produtos 
-          WHERE 
-            id_produto = $1
-            ${ idLoja ? 'and id_loja = $2 ' : ''}
-          ORDER BY
-            id_loja`
-    result = await db.query(sql, params)
-    return ! idLoja ? result.rows : result.rows.length > 0 ? result.rows[0] : {} 
+  static precoVendaPlano(idProduto, idLoja, idTabela, idPlano) {
+    return new Promise((resolve, reject) => {
+      let params, sql
+      params = [idProduto, idLoja, idTabela]
+      if (idPlano) params.push(idPlano)
+      sql = `SELECT 
+              id_plano_pag, 
+              nome_plano_pag,
+              forma_pag,
+              preco_venda
+            FROM 
+              vs_api_produtos_precos_planos 
+            WHERE 
+              id_produto = $1 and id_loja = $2 and id_tab_preco = $3
+              ${ idPlano ? 'and id_plano_pag = $4' : '' }
+            ORDER BY
+              id_plano_pag`
+      db.query(sql, params)
+        .then(({rows}) => resolve(! idPlano ? rows : rows.length > 0 ? rows[0] : {}))
+        .catch(error => reject(error))
+    })
   }
 
 
-  static async estoqueLojaGrade(idProduto, idLoja, posGrade) {
-    let params, sql, result
-    params = [idProduto, idLoja]
-    if (posGrade) params.push(posGrade)
-    sql = `SELECT 
-            pos_grade, 
-            descr_linha,
-            descr_coluna,
-            saldo_loja,
-            saldo_dep,
-            saldo_loja_frc,
-            saldo_dep_frc
-          FROM 
-            vs_api_produtos_grades
-          WHERE 
-            id_produto = $1 and id_loja = $2
-            ${ posGrade ? 'and pos_grade = $3 ' : ''}                  
-          ORDER BY
-            pos_grade`
-    result = await db.query(sql, params)
-    return ! posGrade ? result.rows : result.rows.length > 0 ? result.rows[0] : {} 
+  static estoqueLoja(idProduto, idLoja) {
+    return new Promise((resolve, reject) => {
+      let params, sql
+      params = [idProduto]
+      if (idLoja) params.push(idLoja)
+      sql = `SELECT 
+              id_loja, 
+              nome_loja,
+              saldo_loja,
+              saldo_dep,
+              saldo_loja_frc,
+              saldo_dep_frc
+            FROM 
+              vs_api_produtos 
+            WHERE 
+              id = $1
+              ${ idLoja ? 'and id_loja = $2 ' : ''}
+            ORDER BY
+              id_loja`
+      db.query(sql, params)
+        .then(({rows}) => resolve(! idLoja ? rows : rows.length > 0 ? rows[0] : {}))
+        .catch(error => reject(error))
+    })
   }
 
-  static async load(idProduto) {
-    let produto = new Produto()
-    await produto.getById(idProduto)
-    return produto
+
+  static estoqueLojaGrade(idProduto, idLoja, posGrade) {
+    return new Promise((resolve, reject) => {
+      let params, sql
+      params = [idProduto, idLoja]
+      if (posGrade) params.push(posGrade)
+      sql = `SELECT 
+              pos_grade, 
+              descr_linha,
+              descr_coluna,
+              saldo_loja,
+              saldo_dep,
+              saldo_loja_frc,
+              saldo_dep_frc
+            FROM 
+              vs_api_produtos_grades
+            WHERE 
+              id_produto = $1 and id_loja = $2
+              ${ posGrade ? 'and pos_grade = $3 ' : ''}                  
+            ORDER BY
+              pos_grade`
+      db.query(sql, params)
+        .then(({rows}) => resolve(! posGrade ? rows : rows.length > 0 ? rows[0] : {} ))
+        .catch(error => reject(error))
+    })
+  }
+
+  static load(idProduto, idLoja) {
+    return new Promise((resolve, reject) => {
+      let produto = new Produto()
+      produto.idLoja = idLoja
+      produto.findById(idProduto)
+        .then(found => resolve(found ? produto : {}))
+        .catch(error => reject(error))
+    })
   }
 
 }
